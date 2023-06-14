c9a7a4673ef3559890bb13ed0e887e04e543a62b
Daiyingxue <>
2023-06-14 17:52:12 +0800

    init:初始化项目

---
+++ bujuan-server/src/main/java/com/taoyuan/BujuanLayuiApplication.java
@@ -0,0 +1,13 @@
+package com.taoyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BujuanLayuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BujuanLayuiApplication.class, args);
+    }
+
+}
---
+++ bujuan-server/src/test/java/com/taoyuan/BujuanLayuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taoyuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BujuanLayuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
